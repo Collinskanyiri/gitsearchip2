f0f7e274f8d2c1252da8eb7678719e699032cd6a
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-09-02 17:50:33 +0300

    add app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RepositoriesComponent } from './repository/repository.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    SearchFormComponent,
+    NavBarComponent,
+    RepositoriesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+      FormsModule,
+      HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
